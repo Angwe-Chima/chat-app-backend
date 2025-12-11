403dedf25627ba7efd839b06da2303544f17c915
Angwe Chima <angwechima@gmail.com>
2025-12-11 18:18:45 +0100

    updated wuth controller for the jwe-> token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -106,7 +106,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("token", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (err) {
     console.error("Logout error:", err);
